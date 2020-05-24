110883c43ab552f7e72788fe9e7aa0feb9423bf5
jieyu <code@jieyu.ai>
2020-05-24 16:37:08 +0800

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zillionare/omega',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
     cmdclass={
         'install': InstallCommand
