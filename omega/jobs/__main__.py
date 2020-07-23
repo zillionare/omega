63f882b42124db97c4e4b357ed6f2ec3d2f48641
jieyu-tech <code@jieyu.ai>
2020-07-23 09:21:14 +0800

    omega start jobs doesnt' launch jobs process

--- omega/jobs/__main__.py
+++ omega/jobs/__main__.py
@@ -5,7 +5,17 @@
 Author: Aaron-Yang [code@jieyu.ai]
 Contributors: 
 
+    required coz we want the package is executable
 """
 import logging
 
+import fire
+
+from omega.jobs import start
+
 logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    fire.Fire({
+        "start": start
+    })
