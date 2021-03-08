ed129a106d1a588344c33a585b76d511d1e2f844
jieyu <code@jieyu.ai>
2021-03-08 23:25:27 +0800

    🧪test: using random port for omega/job server during test
    add find_free_port(),  start omega/archive/jobs with random port during test, thus dev.yaml is no need for now

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import logging
 import os
@@ -8,6 +9,9 @@ import time
 
 import aiohttp
 import cfg4py
+
+import socket
+from contextlib import closing
 
 from omega.config import get_config_dir
 
@@ -32,9 +36,9 @@ def init_test_env():
     return cfg
 
 
-async def is_local_omega_alive(port: int = 3181):
+async def is_local_omega_alive():
     try:
-        url = f"http://localhost:{port}/sys/version"
+        url = f"{cfg.omega.urls.quotes_server}/sys/version"
         async with aiohttp.ClientSession() as client:
             async with client.get(url) as resp:
                 if resp.status == 200:
@@ -45,14 +49,8 @@ async def is_local_omega_alive(port: int = 3181):
     return False
 
 
-async def start_omega(port: int = 3181):
-    if await is_local_omega_alive(port):
-        msg = (
-            "omega is running on localhost. However, it may fails the test due to it's"
-            "not started with configurations required by unittest"
-        )
-        logger.warning(msg)
-        return None
+async def start_omega(timeout=60):
+    port = find_free_port()
 
     cfg.omega.urls.quotes_server = f"http://localhost:{port}"
     account = os.environ["JQ_ACCOUNT"]
@@ -75,8 +73,8 @@ async def start_omega(port: int = 3181):
         ],
         env=os.environ,
     )
-    for i in range(60, 0, -1):
-        time.sleep(1)
+    for i in range(timeout, 0, -1):
+        await asyncio.sleep(1)
         if await is_local_omega_alive():
             # return the process id, the caller should shutdown it later
             logger.info("omega sever started: %s", process.pid)
@@ -84,6 +82,33 @@ async def start_omega(port: int = 3181):
 
     os.kill(process.pid, signal.SIGINT)
     raise TimeoutError("Omega server is not started.")
+
+
+async def is_local_job_server_alive(port):
+    try:
+        url = f"http://localhost:{port}/jobs/status"
+        async with aiohttp.ClientSession() as client:
+            async with client.get(url) as resp:
+                if resp.status == 200:
+                    return True
+    except Exception:
+        pass
+
+    return False
+
+
+async def start_job_server(port, timeout=30):
+    process = subprocess.Popen(
+        [sys.executable, "-m", "omega.jobs.main", "start", f"--port={port}"],
+        env=os.environ,
+    )
+
+    for i in range(timeout, 0, -1):
+        await asyncio.sleep(1)
+        if await is_local_job_server_alive(port):
+            return process
+
+    raise TimeoutError("Archieved Bars server not started")
 
 
 async def is_local_archive_server_alive(port):
@@ -99,16 +124,26 @@ async def is_local_archive_server_alive(port):
     return False
 
 
-async def start_archive_server(port=8003):
+async def start_archive_server(timeout=30):
+    port = find_free_port()
+    cfg.omega.urls.archive = f"http://localhost:{port}"
     _dir = os.path.join(os.path.dirname(__file__), "data")
 
     process = subprocess.Popen(
         [sys.executable, "-m", "http.server", "-d", _dir, str(port)]
     )
 
-    for i in range(5, 0, -1):
-        time.sleep(1)
+    for i in range(timeout, 0, -1):
+        await asyncio.sleep(1)
         if await is_local_archive_server_alive(port):
             return process
 
     raise TimeoutError("Archieved Bars server not started")
+
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("localhost", 0))
+        # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        port = s.getsockname()[1]
+        return port
